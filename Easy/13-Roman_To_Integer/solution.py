bfe0635c610748c28aaeff0ad436ed933d80ef54
EldarGafarov <93073346+EldarGafarov@users.noreply.github.com>
2026-04-17 11:16:39 +0300

    Solved 13

---
+++ Easy/13-Roman_To_Integer/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res=0
+        for i in range(len(s)):
+            if i==len(s)-1:
+                res+=roman_dict[s[i]]
+            else:
+                if roman_dict[s[i]]>=roman_dict[s[i+1]]:
+                    res+=roman_dict[s[i]]
+                else:
+                    res-=roman_dict[s[i]]
+        return res
+            
+
+        
