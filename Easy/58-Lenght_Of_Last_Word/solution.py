385de09f03c605e99c6e2cd0b8d17a98ef76115c
EldarGafarov <93073346+EldarGafarov@users.noreply.github.com>
2026-04-17 11:35:06 +0300

    Solved 58

---
+++ Easy/58-Lenght_Of_Last_Word/solution.py
@@ -0,0 +1,28 @@
+
+#Solution 1: Using split() method to split the string into words and then return the length of the last word.
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words=s.split()
+        return len(words[-1])
+    
+
+
+#Solution 2: Using two pointers to traverse the string from the end and count the characters of the last word.
+# class Solution(object):
+#     def lengthOfLastWord(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         i = len(s) - 1
+#         count = 0
+#         while i >= 0 and s[i] == ' ':
+#             i -= 1
+#         while i >= 0 and s[i] != ' ':
+#             count += 1
+#             i -= 1     
+#         return count
